4b458d2feae2755134edc3a86cc8ca217e0e48d4
sathyajitloganathan <loganathansathyajit@gmail.com>
2018-09-14 13:30:39 +0530

    socdocs56-61.json scraping code

--- WebScraping/spiders/SOSpider.py
+++ WebScraping/spiders/SOSpider.py
@@ -26,7 +26,7 @@ class SospiderSpider(scrapy.Spider):
             yield scrapy.Request(abs_n_l)
         """
 
-        for i in range(51,56):
+        for i in range(56,61):
             yield scrapy.Request('https://stackoverflow.com/questions/tagged/python?page='+str(i)+'&sort=votes&pagesize=50'
                                 , callback=self.parse_question_list)
 
